fa44c5fce0c552f047d83eaf8a9d897ed062d989
Adeyemi-theRustyDev <abiadeabdulazeez@gmail.com>
2024-01-18 19:00:48 +0100

    Underestanding how middleware works

--- Program.cs
+++ Program.cs
@@ -3,47 +3,12 @@ using Microsoft.Extensions.Primitives;
 
 var app = WebApplication.CreateBuilder().Build();
 
-app.Run(async (HttpContext context) => {
-    string path = context.Request.Path;
-    string method = context.Request.Method;
-    context.Response.StatusCode = 200;
-    
-    if(path == "/" || path == "/Home")
-    {
-        await context.Response.WriteAsync("You are in the Home Page");
-    }
-    else if(path == "/Contact")
-    {
-        await context.Response.WriteAsync("You are in the Contact Page");
-    }
-    else if(method == "POST" && path == "/Product")
-    {
-        Stream body = context.Request.Body;
-        StreamReader reader = new StreamReader(body);
-        string data = await reader.ReadToEndAsync();
-        string id = string.Empty, name = string.Empty;
-        /* StringValues are used as the values for query parameters because a single key can have multiple values
-        E.g. `Name=Yemi&Age=19&Name=Abdulazeez` */
-        Dictionary<string, StringValues> dataDict = QueryHelpers.ParseQuery(data);
-        if(dataDict.ContainsKey("id")) id = dataDict["id"]!;
-        if(dataDict.ContainsKey("name")) name = dataDict["name"]!;
-        await context.Response.WriteAsync($"ID: {id}; Name: {name}");
-    }
-    else if(path == "/Product")
-    {
-        var query = context.Request.Query;
-        if(query.ContainsKey("id") && query.ContainsKey("name"))
-        {
-            string id = query["id"].ToString();
-            string name = $"{query["name"]}" ?? "";
-            await context.Response.WriteAsync($"You selected the prodcut with ID: {id} and name: {name}");
-            return;
-        }
-        await context.Response.WriteAsync("You are in the products Page");
-    }
-    else{
-        context.Response.StatusCode = 404;
-        await context.Response.WriteAsync("The page you are looking for cannot be found");
-    }
+app.Use(async (context, next) => {
+    var log = context.Request.Path.ToString();
+    Console.WriteLine(log);
+    await next.Invoke();
+});
+app.Run(async context => {
+    await context.Response.WriteAsync("Hello World!");
 });
 app.Run();
