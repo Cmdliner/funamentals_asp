9c82bf05fac03e65799b2ce4f5c75ee8b39e2afe
Adeyemi-theRustyDev <abiadeabdulazeez@gmail.com>
2024-01-18 13:37:53 +0100

    Configuration files have been added

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ app.Run(async (HttpContext context) => {
     string path = context.Request.Path;
     string method = context.Request.Method;
     context.Response.StatusCode = 200;
+    
     if(path == "/" || path == "/Home")
     {
         await context.Response.WriteAsync("You are in the Home Page");
