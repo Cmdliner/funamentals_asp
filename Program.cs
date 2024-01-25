c26dba0c6d532911534a8b90a46d3a6b7d6c1827
Adeyemi-theRustyDev <abiadeabdulazeez@gmail.com>
2024-01-25 06:24:12 +0100

    Get and endpoint

--- Program.cs
+++ Program.cs
@@ -5,6 +5,15 @@ var app = builder.Build();
 
 app.UseRouting();
 
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    Endpoint? endpoint = context.GetEndpoint();
+    if (endpoint.DisplayName != null)
+    {
+        await context.Response.WriteAsync($"{endpoint.DisplayName}\n");
+    }
+    await next(context);
+});
 // Conditional middleware
 app.UseWhen((context) => context.Request.Query.ContainsKey("name"), app => app.UseRequestQuery());
 
